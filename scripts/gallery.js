5507df2bdf27193e8a5b3f39e9c92f09efff0661
Samuel Emeka <samuel.emeka@kibo.school>
2023-09-10 01:35:16 +0100

    new site

---
+++ scripts/gallery.js
@@ -0,0 +1,22 @@
+// var searchField = document.getElementById('search').value;
+var submitButton = document.getElementById('sub-btn');
+
+function Search() {
+    let searchField = document.getElementById('search').value.toUpperCase();
+    let item = document.querySelectorAll('.image-item');
+    let li = document.getElementsByTagName('span');
+
+    for(i = 0; i < li.length; i++){
+        let a = item[i].getElementsByClassName('overlay')[0].getElementsByTagName('span')[0];
+        let value = a.innerHTML || a.innerText || a.textContent;
+
+        if(value.toUpperCase().indexOf(searchField) > -1 ){
+            item[i].style.display = "flex";
+        }else {
+            item[i].style.display = "none";
+        }
+    }
+}
+
+submitButton.addEventListener('click', Search)
+
